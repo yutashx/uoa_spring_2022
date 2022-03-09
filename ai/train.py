2ce95ccdf15ce1c92c05de570383a22a44d3078c
yutashx <yutashx@gmail.com>
2022-03-10 01:48:11 +0900

    update network for 256x256 image

--- ai/train.py
+++ ai/train.py
@@ -13,13 +13,14 @@ import utils
 
 
 class Net(nn.Module):
+    """
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.dropout1 = nn.Dropout2d(0.25)
         self.dropout2 = nn.Dropout2d(0.5)
-        self.fc1 = nn.Linear(9216, 128)
+        self.fc1 = nn.Linear(9216, 128)# 9216 = h * w * channel = 12 * 12 * 64
         self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
@@ -31,6 +32,38 @@ class Net(nn.Module):
         x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+    """
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1, padding="same") #conv2d(in_channels, out_chnnels, kernel_size, stride=1, padding="same")
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv3 = nn.Conv2d(64, 64, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(12544, 128) # = 64 * 15 * 15 = out_channels * pooled_img
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x): # 256
+        x = self.conv1(x) # 254
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2) # 127
+        x = self.conv2(x) # 125
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2) # 63
+        x = self.conv3(x) # 61
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2) # 31 
+        x = self.conv3(x) # 29
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2) # 14
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x) 
         x = F.relu(x)
         x = self.dropout2(x)
         x = self.fc2(x)
