4c01b96f9efb3bfa826b539b4770e6bdbfd8f029
yutashx <yutashx@gmail.com>
2022-03-10 10:14:47 +0900

    delete files

--- ai/train.py
+++ ai/train.py
@@ -46,11 +46,7 @@ class Net(nn.Module):
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
-    """
-    for batch_idx, (data, target) in enumerate(train_loader):
-        print(batch_idx, data, target)
-        data, target = data.to(device), target.to(device)
-    """
+
     for batch_idx, (data, label) in enumerate(train_loader, 0):
         #data = batch["image"]
         #target = batch["label"]
